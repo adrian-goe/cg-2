95eab09d559b063aed9313ebaf483a66780edbba
adriangoerisch <adriangoerisch@outlook.de>
2020-12-17 11:44:54 +0100

    testing stuff

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,10 @@
 // The entry file of your WebAssembly module.
+import {Prime} from "./isPrime";
 
 export function add(a: i32, b: i32): i32 {
-  return a + b;
+    return a + b;
+}
+
+export function isPrime(x: u32): bool {
+    return Prime.isPrime(x);
 }
