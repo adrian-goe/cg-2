dd65e58fb69a96dfef1a8b6763d508d856f2c2a8
adriangoerisch <adriangoerisch@outlook.de>
2020-12-19 13:16:40 +0100

    more testing

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,15 +1,7 @@
 // The entry file of your WebAssembly module.
-import {Prime} from "./isPrime";
 import {Image} from "./drawCross";
 
-export function add(a: i32, b: i32): i32 {
-    return a + b;
-}
 
-export function isPrime(x: u32): bool {
-    return Prime.isPrime(x);
-}
-
-export function cross(input: i32, imageWidth: i32, imageHeight: i32, crossWidth: i32): i32 {
-    return Image.drawCross(input, imageWidth, imageHeight, crossWidth);
+export function cross(byteSize: i32, imageWidth: i32, imageHeight: i32, crossWidth: i32): i32 {
+    return Image.newDrawCross(byteSize, imageWidth, imageHeight, crossWidth);
 }
